c743eeac53fa89b6481f016ff40692f1d7538b3a
ayham <aehamkefo2003@gmail.com>
2025-12-08 15:51:20 +0300

    add tne get event method to the cubit

--- lib/features/events/presentation/cubit/event_cubit/event_cubit.dart
+++ lib/features/events/presentation/cubit/event_cubit/event_cubit.dart
@@ -2,8 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 import 'package:lawaen/app/core/utils/enums.dart';
+import 'package:lawaen/app/location_manager/location_service.dart';
+import 'package:lawaen/features/events/data/models/event_model.dart';
 import 'package:lawaen/features/events/data/models/event_type_model.dart';
 import 'package:lawaen/features/events/data/repos/event_repo.dart';
+import 'package:lawaen/features/events/presentation/params/get_events_params.dart';
 
 part 'event_state.dart';
 
@@ -11,7 +14,9 @@ part 'event_state.dart';
 class EventCubit extends Cubit<EventState> {
   final EventRepo _eventRepo;
 
-  EventCubit(this._eventRepo) : super(const EventState());
+  final LocationService _locationService;
+
+  EventCubit(this._eventRepo, this._locationService) : super(const EventState());
 
   Future<void> getEventTypes() async {
     emit(state.copyWith(eventTypesState: RequestState.loading, eventTypesError: null));
@@ -25,5 +30,28 @@ class EventCubit extends Cubit<EventState> {
         emit(state.copyWith(eventTypesState: RequestState.success, eventTypes: types, eventTypesError: null));
       },
     );
+  }
+
+  Future<void> getEvents({required String eventTypeId}) async {
+    emit(state.copyWith(eventsState: RequestState.loading, eventsError: null));
+
+    final cityId = await _getUserCity();
+    final params = GetEventsParams(city: cityId);
+
+    final result = await _eventRepo.getEvents(params: params, eventTypeId: eventTypeId);
+
+    result.fold(
+      (failure) {
+        emit(state.copyWith(eventsState: RequestState.error, eventsError: failure.errorMessage));
+      },
+      (events) {
+        emit(state.copyWith(eventsState: RequestState.success, events: events, eventsError: null));
+      },
+    );
+  }
+
+  Future<String> _getUserCity() async {
+    final userLocation = await _locationService.getBestEffortLocation();
+    return userLocation.cityId ?? "";
   }
 }
--- lib/features/events/presentation/cubit/event_cubit/event_state.dart
+++ lib/features/events/presentation/cubit/event_cubit/event_state.dart
@@ -4,25 +4,37 @@ class EventState extends Equatable {
   final RequestState eventTypesState;
   final List<EventTypeModel> eventTypes;
   final String? eventTypesError;
+  final RequestState eventsState;
+  final List<EventModel> events;
+  final String? eventsError;
 
   const EventState({
     this.eventTypesState = RequestState.idle,
     this.eventTypes = const [],
     this.eventTypesError,
+    this.eventsState = RequestState.idle,
+    this.events = const [],
+    this.eventsError,
   });
 
   EventState copyWith({
     RequestState? eventTypesState,
     List<EventTypeModel>? eventTypes,
     String? eventTypesError,
+    RequestState? eventsState,
+    List<EventModel>? events,
+    String? eventsError,
   }) {
     return EventState(
       eventTypesState: eventTypesState ?? this.eventTypesState,
       eventTypes: eventTypes ?? this.eventTypes,
       eventTypesError: eventTypesError,
+      eventsState: eventsState ?? this.eventsState,
+      events: events ?? this.events,
+      eventsError: eventsError,
     );
   }
 
   @override
-  List<Object?> get props => [eventTypesState, eventTypes, eventTypesError];
+  List<Object?> get props => [eventTypesState, eventTypes, eventTypesError, eventsState, events, eventsError];
 }
