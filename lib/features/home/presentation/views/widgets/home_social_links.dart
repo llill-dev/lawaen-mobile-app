fe2bb4b7f672dc2c75879e195587729b253bf5c3
ayham <aehamkefo2003@gmail.com>
2025-12-14 13:20:49 +0300

    fix home socail links

--- lib/features/home/presentation/views/widgets/home_social_links.dart
+++ lib/features/home/presentation/views/widgets/home_social_links.dart
@@ -22,25 +22,21 @@ class HomeSocialLinks extends StatelessWidget {
         if (state.contactState == RequestState.loading) {
           return SliverToBoxAdapter(child: _buildSkeletonLoader());
         }
+
         if (state.contactState == RequestState.error ||
             (state.contact.isEmpty && state.contactState == RequestState.success)) {
-          return SliverToBoxAdapter(child: SizedBox.shrink());
+          return const SliverToBoxAdapter(child: SizedBox.shrink());
         }
+
         return SliverToBoxAdapter(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(LocaleKeys.followUsOnSocialMedia.tr(), style: Theme.of(context).textTheme.headlineMedium),
-              SizedBox(height: 8.h),
-              SizedBox(
-                height: 40.h,
-                child: ListView.separated(
-                  padding: EdgeInsets.zero,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: state.contact.length,
-                  separatorBuilder: (_, _) => SizedBox(width: 20.w),
-                  itemBuilder: (context, index) => _buildLinkItem(item: state.contact[index]),
-                ),
+              SizedBox(height: 12.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: state.contact.map((item) => _buildLinkItem(item: item)).toList(),
               ),
             ],
           ).horizontalPadding(padding: 16.w),
@@ -52,43 +48,33 @@ class HomeSocialLinks extends StatelessWidget {
   Widget _buildLinkItem({required ContactModel item}) {
     return GestureDetector(
       onTap: () => launchURL(link: item.url),
-      child: Center(
-        child: CachedImage(url: item.image, width: 32.r, height: 32.r),
-      ),
+      child: CachedImage(url: item.image, width: 32.r, height: 32.r),
     );
   }
-}
 
-Widget _buildSkeletonLoader() {
-  return Padding(
-    padding: EdgeInsets.symmetric(horizontal: 16.w),
-    child: SizedBox(
-      height: 0.12.sh,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: 4,
-        separatorBuilder: (_, _) => SizedBox(width: 8.w),
-        itemBuilder: (_, _) => const _ContactSkeleton(),
-      ),
-    ),
-  );
-}
-
-class _ContactSkeleton extends StatelessWidget {
-  const _ContactSkeleton();
-
-  @override
-  Widget build(BuildContext context) {
-    return Skeletonizer(
-      enabled: true,
-      child: Column(
-        children: [
-          Container(
-            width: 25.r,
-            height: 25.r,
-            decoration: BoxDecoration(shape: BoxShape.circle, color: ColorManager.lightGrey),
-          ),
-        ],
+  Widget _buildSkeletonLoader() {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Skeletonizer(
+              enabled: true,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(
+                  4,
+                  (_) => Container(
+                    width: 25.r,
+                    height: 25.r,
+                    decoration: const BoxDecoration(shape: BoxShape.circle, color: ColorManager.lightGrey),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
